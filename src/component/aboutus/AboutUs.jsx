0a64ed025540fe04be55f626f68f6f9efdffc849
dhakarvikas <vikas.dhakar05@gmail.com>
2024-09-09 19:51:47 +0530

    ourhistorysection

--- src/component/aboutus/AboutUs.jsx
+++ src/component/aboutus/AboutUs.jsx
@@ -1,11 +1,13 @@
 import AboutCompany from "./AboutCompany";
 import FirstAbout from "./FirstAbout";
+import OurHistorySection from "./OurHistorySection";
 
 const AboutUs=()=>{
     return(
         <>
         <FirstAbout />
         <AboutCompany />
+        <OurHistorySection />
         </>
     )
 };
